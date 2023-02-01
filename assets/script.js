fbd59a0e1165162cff711d1fdd7eafe7958b3688
ClaudioF9 <claudioferreira0284@gmail.com>
2023-02-01 00:04:38 +0000

    Able to fix background colour change, cleaned a bit of code and made it neater, changed id for several code

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
 //create variables
 let dateAndTime = $("#currentDay");
+let blockTime = $(".time-block");
+
+let testingTime = $(".hour").attr("id");
+console.log(testingTime);
 
 
 //create function to display time-------------------
@@ -11,114 +15,51 @@ dateAndTime.text(currentTime);
 timeDisplay();
 //---------------------------------------------------
 
-//creating the text input
-// $(document).ready(function() {
-//     $('.description').click(function() {
-//       // Check if the description text is not already an input
-//       if ($(this).children().length === 0) {
-//         // Create an input and set the value to the current text
-//         var input = $('<input>').val($(this).text());
-//         // Replace the current text with the input
-//         $(this).html(input);
-//         // Focus on the input so the user can type immediately
-//         input.focus();
-//       }
-//     });
-//   });
 
-//uses click to allow for text to be typed in
+//Change backgournd colour------------------------What a mission
+let currentHour = moment().format("H");
+// let currentHour = "13"; 
+$('.description').each(function(){
+    var idValue = parseInt($(this).prop('id'));
+    if(idValue > currentHour && idValue < currentHour+6){
+        $(this).css('background-color','#77dd77');
+    }else if(idValue < currentHour && idValue > currentHour-6){
+        $(this).css('background-color','#d3d3d3');
+    }else if(idValue == currentHour){
+        $(this).css('background-color','#ff6961');
+    }
+});
+
+// console.log(typeof currentHour);
+
+
+
+//uses click to allow for text to be typed in----------------------
 $(document).ready(function() {
     $('.description').click(function() {
-      $(this).attr('contentEditable', 'true');
+    $(this).attr('contentEditable', 'true');
     });
-  });
+});
 
-//save to local storage
+//save to local storage----------------------------------------------
 $(".saveBtn").click(function () {
     let text = $(this).prev().text();
     let time = $(this).prev().prev().attr("id");
     localStorage.setItem(time, text);
+    console.log(typeof time);
     });
 
-    //not working
-    // $(document).ready(function() {
-    //     let times = $('.description').prev().attr("id");
-    //     let savedText = sessionStorage.getItem(times);
-    //     if (savedText) {
-    //     $('.description').text(savedText);
-    //     }
-    //     });
+//stores text input in local storage---------------------------------
+$(".saveBtn").click(function () {
+let text = $(this).prev().text();
+let time = $(this).prev().prev().attr("id");
+localStorage.setItem(time, text);
+});
 
-//workign but not idea
-    // $(document).ready(function() {
-    // let items = Object.entries(localStorage);
-    // for (const [time, text] of items) {
-    // $("#" + time).next().text(text);
-    // }
-    // });
-
-// $(document).ready(function() {
-//     $(".description").each(function() {
-//         let hour = $(this).siblings(".hour").text();
-//         let description = localStorage.getItem(hour);
-//         $(this).val(description);
-//     });
-// });
-      
-      
-      
-      
-
-// get item if needed
-//$(".saveBtn").click(function () {
-// let text = $(this).prev().text();
-// let time = $(this).prev().prev().attr("id");
-// localStorage.setItem(time, text);
-// });
-
-
-
-// let testTime = moment().format("H");
-// console.log(testTime);
-
-// let testId = $("#16");
-
-// console.log(typeof testId);
-
-// let testFinal = JSON.stringify(testId);
-// console.log(testFinal);
-
-// console.log(typeof testTime);
-
-// let test = parseInt(testId);
-// console.log(test);
-
-// let currentHour = parseInt(moment().format('H'));
-// console.log(currentHour);
-// console.log(typeof currenthour);
-
-// console.log(testId);
-
-// let textId = testId.text();
-// let testStr = textId.toString();
-
-// console.log(testStr);
-
-// let newTest = $("#16");
-// let newStr = newTest.toString();
-// console.log(newStr);
-
-// let testIng = $("elementSelector").attr("16");
-// console.log(testIng);
-
-// let something = $("#17").val("17");
-// console.log(something);
-
-// testTwo = $("#13").data("key", "13");
-// console.log($("#13").data("value"));
-
-// let tryIng = $("div").attr("id");
-// console.log(tryIng);
-
-// let tyrinAgain = $("9").val();
-// console.log(tyrinAgain);
+//retains text input after refresh-------------------------------------
+$(document).ready(function() {
+    let items = Object.entries(localStorage);
+    for (const [time, text] of items) {
+    $("#" + time).next().text(text);
+    }
+    });
